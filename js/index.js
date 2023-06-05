e1891a0dce5fc6899a2be47627d83d3b41fd645a
DenisAnf <ad.vox3@gmail.com>
2023-06-06 02:56:01 +0300

    added conter value post

--- js/index.js
+++ js/index.js
@@ -18,10 +18,31 @@ const postLengthError = document.querySelector('#error');
 
 //указание лимитов на количество символов в посте
 const TITLE_MAX_LENGTH_VALUE = 10;
-const DISCRIPRION_MAX_LENGTH_VALUE = 10;
+const DISCRIPRION_MAX_LENGTH_VALUE = 20;
 
 //указание пустого массива для ленты постов
 let feed = [];
+
+
+//функции вывода счетчиков символов
+function showPostTitleLengthCounter() {
+	postTitleLengthCounter.innerHTML = `Использовано ${postTitleNode.value.length} символов из ${TITLE_MAX_LENGTH_VALUE} доступных`
+}
+showPostTitleLengthCounter();
+postTitleNode.addEventListener('input', showPostTitleLengthCounter);
+
+function showPostDiscriptionLengthCounter() {
+	postDiscriptionLengthCounter.innerHTML = `Использовано ${postDiscriptionNode.value.length} символов из ${DISCRIPRION_MAX_LENGTH_VALUE} доступных`
+}
+showPostDiscriptionLengthCounter();
+postDiscriptionNode.addEventListener('input', showPostDiscriptionLengthCounter);
+
+
+
+
+
+
+
 
 submitButton.addEventListener('click', function () {
 	postsFeed.innerText = `${postTitleNode.value} ${postDiscriptionNode.value}`;
