42b0833b28a04d97ebeda55a519a227f8ee44b56
DenisAnf <ad.vox3@gmail.com>
2023-06-07 05:47:32 +0300

    added scroll & function addPost

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-//определение поста
+//определение полей ввода поста
 const postTitleNode = document.querySelector('#inputTitle');
 const postDiscriptionNode = document.querySelector('#inputDiscription');
 
@@ -7,7 +7,7 @@ const submitButton = document.querySelector('#buttonSubmit');
 const resetButton = document.querySelector('#buttonReset');
 
 //определение места вывода постов
-const postsFeed = document.querySelector('#feed');
+const postsFeedPlace = document.querySelector('#feed');
 
 //определение места вывода счетчиков символов
 const postTitleLengthCounter = document.querySelector('#inputTitleLengthCounter');
@@ -17,33 +17,110 @@ const postDiscriptionLengthCounter = document.querySelector('#inputDiscriptionLe
 const postLengthError = document.querySelector('#error');
 
 //указание лимитов на количество символов в посте
-const TITLE_MAX_LENGTH_VALUE = 10;
-const DISCRIPRION_MAX_LENGTH_VALUE = 20;
+const TITLE_LENGTH_MAX_VALUE = 10;
+const DISCRIPRION_LENGTH_MAX_VALUE = 20;
 
 //указание пустого массива для ленты постов
-let feed = [];
+let feedPosts = [];
 
+//! ФУНКЦИИ -------------------------------------------------------
+
+//функция-конструктор поста
+function Post(title, discription) {
+	this.title = title;
+	this.discription = discription;
+}
+
+//функция получения поста
+function getPostFromUser() {
+	const titlePost = postTitleNode.value;
+	const discriptionPost = postDiscriptionNode.value;
+
+	const post = new Post(titlePost, discriptionPost);
+
+	return post;
+}
+
+//функция добавления поста в массив
+function addPost(post) {
+	feedPosts.push(post);
+}
+
+//функция получения массива постов
+function getPostsFeed() {
+	return feedPosts.reverse();
+}
+
+//функция получения текущей даты и времени
+function getCurrentDate() {
+	const date = new Date();
+	
+	return date.toLocaleString('ru-RU', {
+		year: 'numeric',
+		month: 'numeric',
+		day: 'numeric',
+		}) + ' ' +
+		date.toLocaleString('ru-RU', {
+			hour: 'numeric',
+			minute: 'numeric',
+		});
+}
+
+//функция показа ленты постов из массива
+function renderPosts() {
+	const showPostsFeed = getPostsFeed();
+
+	let showPostsHTML = '';
+
+	for (let i = 0; i < showPostsFeed.length; i++) {
+		showPostsHTML += `
+		<div class="post">
+			<p class="post__date">${getCurrentDate()}</p> 
+			<h3 class="post__title">${showPostsFeed[i].title}</h3>
+			<p class="post__discription">${showPostsFeed[i].discription}</p>
+		</div>
+		`;
+	}
+
+	postsFeedPlace.innerHTML = showPostsHTML;
+}
+
+//функция очистки полей после добавления поста
+function clearPostPlace() {
+	postTitleNode.value = null;
+	postDiscriptionNode.value = null;
+}
 
 //функции вывода счетчиков символов
 function showPostTitleLengthCounter() {
-	postTitleLengthCounter.innerHTML = `Использовано ${postTitleNode.value.length} символов из ${TITLE_MAX_LENGTH_VALUE} доступных`
+	postTitleLengthCounter.innerText = `Использовано ${postTitleNode.value.length} символов из ${TITLE_LENGTH_MAX_VALUE}`
 }
-showPostTitleLengthCounter();
-postTitleNode.addEventListener('input', showPostTitleLengthCounter);
 
 function showPostDiscriptionLengthCounter() {
-	postDiscriptionLengthCounter.innerHTML = `Использовано ${postDiscriptionNode.value.length} символов из ${DISCRIPRION_MAX_LENGTH_VALUE} доступных`
+	postDiscriptionLengthCounter.innerText = `Использовано ${postDiscriptionNode.value.length} символов из ${DISCRIPRION_LENGTH_MAX_VALUE}`
 }
-showPostDiscriptionLengthCounter();
-postDiscriptionNode.addEventListener('input', showPostDiscriptionLengthCounter);
+
+//функция очистки счетчика после добавления поста
+function clearPostLengthCounter() {
+	postTitleLengthCounter.innerText = null;
+	postDiscriptionLengthCounter.innerText = null;
+}
 
 
 
+//! ОБРАБОТЧИКИ ---------------------------------------------------
 
-
-
-
-
+//Обработка кнопки добавления поста
 submitButton.addEventListener('click', function () {
-	postsFeed.innerText = `${postTitleNode.value} ${postDiscriptionNode.value}`;
+	const postFromUser = getPostFromUser();
+
+	addPost(postFromUser)
+	renderPosts();
+	clearPostPlace();
+	clearPostLengthCounter();
+
 });
+
+//Вывод счетчиков сиволов
+postTitleNode.addEventListener('input', showPostTitleLengthCounter);
+postDiscriptionNode.addEventListener('input', showPostDiscriptionLengthCounter);
